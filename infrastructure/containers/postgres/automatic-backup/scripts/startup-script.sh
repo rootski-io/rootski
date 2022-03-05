3d33835bcbaf5e6a4be01ca902ff0a61daf27026
Isaac <ir3456@gmail.com>
2022-03-05 14:13:02 -0700

    removed -m xonsh from all isnttances where backup_or_restore.py is run because it is a python file

--- infrastructure/containers/postgres/automatic-backup/scripts/startup-script.sh
+++ infrastructure/containers/postgres/automatic-backup/scripts/startup-script.sh
@@ -13,9 +13,9 @@ echo "Running /scripts/wait-for-postgres-init.sh"
 bash scripts/wait-for-postgres-init.sh
 
 # Restores the database from the latest S3 backup
-echo "Running python3 -m xonsh backup_or_restore.py restore-from-most-recent"
-python3 -m xonsh backup_or_restore.py restore-from-most-recent
+echo "Running python3 -m backup_or_restore.py restore-from-most-recent"
+python3 backup_or_restore.py restore-from-most-recent
 
 # Backs up the database to S3 regularly according to the $BACKUP_INTERVAL
-echo "Running python3 -m xonsh backup_or_restore.py backup-on-interval"
-python3 -m xonsh backup_or_restore.py backup-on-interval
+echo "Running python3 -m backup_or_restore.py backup-on-interval"
+python3 backup_or_restore.py backup-on-interval
