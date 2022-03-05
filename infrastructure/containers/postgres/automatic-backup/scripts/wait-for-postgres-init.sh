e378a7bf2ee2637f8bc04c5ae9b4d1e90e5a2f58
Isaac <ir3456@gmail.com>
2022-03-05 00:14:05 -0700

    updated logic in wait-for-postgres-init.sh

--- infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
+++ infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # wait-for-postgres-init.sh
 
-# Checks the container until the database has been fully initialized
-until pg_isready --username ${POSTGRES_USER} --dbname ${POSTGRES_DB} --port ${POSTGRES_PORT}; do
+# Checks the container logs until the database has been fully initialize
+until psql -lw -U $POSTGRES_USER --host $POSTGRES_HOST -d $POSTGRES_DB | grep $POSTGRES_DB; do
   >&2 echo "Postgres initialization in progress - sleeping"
   sleep 1
 done
