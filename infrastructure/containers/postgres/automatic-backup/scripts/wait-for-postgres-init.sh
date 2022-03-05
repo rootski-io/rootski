740d82b25298ddd3f8abd074ea3d81080855eed2
Isaac <ir3456@gmail.com>
2022-03-04 20:42:11 -0700

    added wait time to wait-for-postgres-init.sh

--- infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
+++ infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
@@ -4,8 +4,10 @@
 # Checks the container logs until the database has been fully initialize
 until pg_isready -U $POSTGRES_USER --host $POSTGRES_HOST -d $POSTGRES_DB; do
   >&2 echo "Postgres initialization in progress - sleeping"
-  sleep 3
+  sleep 1
 done
+
+sleep 5
 
 >&2 echo "Postgres is initialized - Ready to restore"
 exec "$@"
