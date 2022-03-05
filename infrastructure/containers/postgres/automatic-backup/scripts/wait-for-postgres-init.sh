7f5797a49209905a8bb7ed42b31ac5e3000454b5
Isaac <ir3456@gmail.com>
2022-03-05 15:32:17 -0700

    removed unnecessary code from wait-for-postgres-init.sh

--- infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
+++ infrastructure/containers/postgres/automatic-backup/scripts/wait-for-postgres-init.sh
@@ -3,11 +3,10 @@
 
 # Checks the container logs until the database has been fully initialize
 until psql -lw -U $POSTGRES_USER --host $POSTGRES_HOST -d $POSTGRES_DB | grep $POSTGRES_DB; do
-  >&2 echo "Postgres initialization in progress - sleeping"
+  echo "Postgres initialization in progress - sleeping"
   sleep 1
 done
 
 sleep 3
 
->&2 echo "Postgres is initialized - Ready to restore"
-exec "$@"
+echo "Postgres is initialized - Ready to restore"
