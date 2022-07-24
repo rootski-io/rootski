591721ba491cbb7c44b7bd7a7c040c5235c9874c
Eric Riddoch <eric.riddoch@bengroup.com>
2022-07-24 02:26:33 -0600

    copied boilerplate python package files from rootski_api

---
+++ dynamo-db/setup.py
@@ -0,0 +1,22 @@
+"""
+Setup file for the rootski package.
+
+Use setup.cfg to configure your project.
+
+This file was generated with PyScaffold 4.0.2.
+PyScaffold helps you to put up the scaffold of your new Python project.
+Learn more under: https://pyscaffold.org/
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        use_pyscaffold=True,
+        # use_scm_version={
+        #     "root": ".", # path to project root from "relative_to"
+        #     "relative_to": __file__, # path to setup.py
+        #     "local_scheme": local_scheme,
+        #     "version_scheme": version_scheme,
+        # }
+    )
