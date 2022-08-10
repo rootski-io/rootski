517f7054556c97a8310e388c338fcc6713491a54
Joseph Drapeau <joseph.p.drapeau@gmail.com>
2022-08-10 10:19:07 -0600

    updated etl

--- dynamo-db/src/dynamodb_play/etl/breakdowns.py
+++ dynamo-db/src/dynamodb_play/etl/breakdowns.py
@@ -52,8 +52,8 @@ def make_dynamo_breakdown_item_dict_from_orm(
             position=str(orm_breakdown_item.position),
             submitted_by_user_email=orm_breakdown_item.breakdown.submitted_by_user_email,  # None if not user_or_none else user_or_none.email,
             morpheme=str(orm_breakdown_item.morpheme),
-            morpheme_family_id=orm_breakdown_item.morpheme_id,  # None if not family_id_or_none else family_id_or_none.family_id,
             morpheme_id=str(orm_breakdown_item.morpheme_id),
+            morpheme_family_id=orm_breakdown_item.morpheme_id,  # None if not family_id_or_none else family_id_or_none.family_id,
             breakdown_id=str(orm_breakdown_item.breakdown_id),
         )
 
@@ -94,7 +94,7 @@ def transform(orm_breakdowns) -> List[dict]:
     breakdown_item_dict_list: List[dict] = [bi.to_item() for bi in dynamo_breakdown_items]
 
     # Return all the dictionaries in a single list
-    return breakdown_dict_list + breakdown_item_dict_list
+    return breakdown_item_dict_list + breakdown_dict_list
 
 
 if __name__ == "__main__":
