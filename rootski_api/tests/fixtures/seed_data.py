d56d4ae4828891219758219d04ae7583194a0046
Joseph Drapeau <joseph.p.drapeau@gmail.com>
2022-08-19 18:02:23 -0600

    Added unit tests and examples in seed_data.py

--- rootski_api/tests/fixtures/seed_data.py
+++ rootski_api/tests/fixtures/seed_data.py
@@ -18,7 +18,7 @@ EXAMPLE_BREAKDOWN = {
     "is_inference": True,
     "date_submitted": "2022-02-15 05:45:18.740114",
     "date_verified": "None",
-    "breakdown_items": [{"position": "0", "morpheme_id": None, "morpheme": "мочь", "morpheme_family_id": None}],
+    "breakdown_items": [{"position": 0, "morpheme_id": None, "morpheme": "мочь", "morpheme_family_id": None}],
 }
 
 
@@ -38,28 +38,116 @@ EXAMPLE_BREAKDOWN_2 = {
     "date_submitted": "2022-02-15 05:45:18.740114",
     "date_verified": "None",
     "breakdown_items": [
-        {"position": "0", "morpheme": "занавеш", "morpheme_id": None, "morpheme_family_id": None},
-        {"position": "1", "morpheme": "ивать", "morpheme_id": None, "morpheme_family_id": None},
+        {"position": 0, "morpheme": "занавеш", "morpheme_id": None, "morpheme_family_id": None},
+        {"position": 1, "morpheme": "ивать", "morpheme_id": None, "morpheme_family_id": None},
     ],
 }
 
 
-EXAMPLE_VERIFiED_BREAKDOWN = {
-    "pk": "WORD#32",
+EXAMPLE_BREAKDOWN_ANOTHER_USER_2 = {
+    "pk": "WORD#5",
     "sk": "BREAKDOWN",
-    "gsi1pk": "USER#anonymous",
-    "gsi1sk": "WORD#32",
-    "gsi2pk": "WORD#32",
-    "gsi2sk": "USER#anonymous",
+    "gsi1pk": "USER#another_user@gmail.com",
+    "gsi1sk": "WORD#5",
+    "gsi2pk": "WORD#5",
+    "gsi2sk": "USER#another_user@gmail.com",
     "__type": "BREAKDOWN",
-    "word": "мочь",
-    "word_id": "32",
-    "submitted_by_user_email": "anonymous",
+    "word": "занавешивать",
+    "word_id": "5",
+    "submitted_by_user_email": "another_user@gmail.com",
     "is_verified": True,
-    "is_inference": True,
+    "is_inference": False,
     "date_submitted": "2022-02-15 05:45:18.740114",
     "date_verified": "None",
-    "breakdown_items": [{"position": "0", "morpheme_id": None, "morpheme": "мочь", "morpheme_family_id": None}],
+    "breakdown_items": [
+        {"position": 0, "morpheme": "занавеш", "morpheme_id": None, "morpheme_family_id": None},
+        {"position": 1, "morpheme": "ивать", "morpheme_id": None, "morpheme_family_id": None},
+    ],
+}
+
+
+EXAMPLE_BREAKDOWN_57 = {
+    "gsi2sk": "USER#anonymous",
+    "submitted_by_user_email": "anonymous",
+    "gsi1sk": "WORD#57",
+    "date_verified": "None",
+    "__type": "BREAKDOWN",
+    "word_id": "57",
+    "date_submitted": "2022-02-15 05:45:18.740114",
+    "word": "знать",
+    "sk": "BREAKDOWN",
+    "pk": "WORD#57",
+    "breakdown_items": [
+        {"morpheme": "зна", "morpheme_family_id": "245", "position": 0, "morpheme_id": "511"},
+        {"morpheme": "ть", "morpheme_family_id": "1385", "position": 1, "morpheme_id": "2196"},
+    ],
+    "gsi1pk": "USER#anonymous",
+    "is_verified": False,
+    "is_inference": True,
+    "gsi2pk": "WORD#57",
+}
+
+
+EXAMPLE_BREAKDOWN_ANOTHER_USER = {
+    "pk": "WORD#10",
+    "sk": "BREAKDOWN",
+    "gsi1pk": "USER#another_user@gmail.com",
+    "gsi1sk": "WORD#10",
+    "gsi2pk": "WORD#10",
+    "gsi2sk": "USER#another_user@gmail.com",
+    "__type": "BREAKDOWN",
+    "word": "занавешивать",
+    "word_id": "10",
+    "submitted_by_user_email": "another_user@gmail.com",
+    "is_verified": False,
+    "is_inference": False,
+    "date_submitted": "2022-02-15 05:45:18.740114",
+    "date_verified": "None",
+    "breakdown_items": [
+        {"position": 0, "morpheme": "занавеш", "morpheme_id": None, "morpheme_family_id": None},
+        {"position": 1, "morpheme": "ивать", "morpheme_id": None, "morpheme_family_id": None},
+    ],
+}
+
+
+EXAMPLE_BREAKDOWN_ERIC_USER = {
+    "pk": "WORD#5",
+    "sk": "BREAKDOWN",
+    "gsi1pk": "USER#eric.riddoch@gmail.com",
+    "gsi1sk": "WORD#5",
+    "gsi2pk": "WORD#5",
+    "gsi2sk": "USER#eric.riddoch@gmail.com",
+    "__type": "BREAKDOWN",
+    "word": "занавешивать",
+    "word_id": "5",
+    "submitted_by_user_email": "eric.riddoch@gmail.com",
+    "is_verified": True,
+    "is_inference": False,
+    "date_submitted": "2022-02-15 05:45:18.740114",
+    "date_verified": "None",
+    "breakdown_items": [
+        {"position": 0, "morpheme": "занавеш", "morpheme_id": None, "morpheme_family_id": None},
+        {"position": 1, "morpheme": "ивать", "morpheme_id": None, "morpheme_family_id": None},
+    ],
+}
+
+
+EXAMPLE_VERIFIED_BREAKDOWN = {
+    "pk": "WORD#30",
+    "sk": "BREAKDOWN",
+    "gsi1pk": "USER#eric.riddoch@gmail.com",
+    "gsi1sk": "WORD#30",
+    "gsi2pk": "WORD#30",
+    "gsi2sk": "USER#eric.riddoch@gmail.com",
+    "__type": "BREAKDOWN",
+    "word": "мочь",
+    "word_id": "30",
+    "submitted_by_user_email": "eric.riddoch@gmail.com",
+    "is_verified": True,
+    "is_inference": False,
+    "date_submitted": "2022-02-15 05:45:18.740114",
+    "date_verified": "None",
+    "breakdown_items": [{"position": 0, "morpheme_id": None, "morpheme": "мочь", "morpheme_family_id": None}],
 }
 
 
@@ -73,7 +161,7 @@ EXAMPLE_BREAKDOWN_ITEM_1 = {
     "sk": "BREAKDOWN_ITEM#2213#0",
     "morpheme_family_id": "2213",
     "pk": "WORD#771",
-    "position": "0",
+    "position": 0,
     "morpheme": "вы",
     "gsi1pk": "MORPHEME_FAMILY#2213",
 }
@@ -84,7 +172,7 @@ EXAMPLE_NULL_BREAKDOWN_ITEM_1 = {
     "sk": "BREAKDOWN_ITEM#f4ef8326-a27e-4cb6-9cac-0089fc1eda45#8",
     "__type": "BREAKDOWN_ITEM_NULL",
     "word_id": "9203",
-    "position": "8",
+    "position": 8,
     "morpheme": "ся",
     "morpheme_id": None,
     "morpheme_family_id": None,
@@ -92,12 +180,44 @@ EXAMPLE_NULL_BREAKDOWN_ITEM_1 = {
 }
 
 
+EXAMPLE_MORPHEME_FAMILY_245 = {
+    "morphemes": [{"morpheme": "знай", "morpheme_id": "510"}, {"morpheme": "зна", "morpheme_id": "511"}],
+    "family_meanings": ["know"],
+    "level": 1,
+    "__type": "MORPHEME_FAMILY",
+    "sk": "MORPHEME_FAMILY#245",
+    "word_pos": "any",
+    "family_id": "245",
+    "pk": "MORPHEME_FAMILY#245",
+    "type": "root",
+}
+
+
+EXAMPLE_MORPHEME_FAMILY_1385 = {
+    "morphemes": [{"morpheme": "ть", "morpheme_id": "2196"}],
+    "family_meanings": [None],
+    "level": 6,
+    "__type": "MORPHEME_FAMILY",
+    "sk": "MORPHEME_FAMILY#1385",
+    "word_pos": "verb",
+    "family_id": "1385",
+    "pk": "MORPHEME_FAMILY#1385",
+    "type": "suffix",
+}
+
+
 EXAMPLE_DATA = [
     EXAMPLE_BREAKDOWN,
     EXAMPLE_BREAKDOWN_2,
-    EXAMPLE_VERIFiED_BREAKDOWN,
+    EXAMPLE_BREAKDOWN_57,
+    EXAMPLE_BREAKDOWN_ANOTHER_USER,
+    EXAMPLE_BREAKDOWN_ANOTHER_USER_2,
+    EXAMPLE_BREAKDOWN_ERIC_USER,
+    EXAMPLE_VERIFIED_BREAKDOWN,
     EXAMPLE_BREAKDOWN_ITEM_1,
     EXAMPLE_NULL_BREAKDOWN_ITEM_1,
+    EXAMPLE_MORPHEME_FAMILY_245,
+    EXAMPLE_MORPHEME_FAMILY_1385,
 ]
 
 
@@ -105,4 +225,3 @@ EXAMPLE_DATA = [
 def seed_data(rootski_dynamo_table: _Table) -> None:
     for data in EXAMPLE_DATA:
         rootski_dynamo_table.put_item(Item=data)
-    # item = rootski_dynamo_table.get_item(Key={"pk": "WORD#32", "sk": "BREAKDOWN"})
--- rootski_api/tests/unit_tests/services/database/dynamo/models2schemas/test__breakdown_.py
+++ rootski_api/tests/unit_tests/services/database/dynamo/models2schemas/test__breakdown_.py
@@ -1,42 +1,49 @@
-from rootski.services.database.dynamo.actions.verified_breakdown import (
+from rootski.services.database.dynamo.actions.breakdown_actions import (
+    get_breakdown_submitted_by_user_email_and_word_id,
+    get_morpheme_family,
     get_official_breakdown_by_word_id,
-    is_verified,
+    get_official_breakdown_submitted_by_another_user,
+    is_breakdown_verified,
 )
 from rootski.services.database.dynamo.db_service import DBService
 from rootski.services.database.dynamo.models.breakdown import Breakdown
 from rootski.services.database.dynamo.models.breakdown_item import make_dynamo_breakdown_item_from_dict
-from tests.fixtures.seed_data import EXAMPLE_DATA, seed_data
+from rootski.services.database.dynamo.models.morpheme_family import MorphemeFamily
+from tests.fixtures.seed_data import (
+    EXAMPLE_BREAKDOWN,
+    EXAMPLE_BREAKDOWN_57,
+    EXAMPLE_BREAKDOWN_ANOTHER_USER,
+    EXAMPLE_BREAKDOWN_ERIC_USER,
+    EXAMPLE_BREAKDOWN_ITEM_1,
+    EXAMPLE_DATA,
+    EXAMPLE_MORPHEME_FAMILY_245,
+    EXAMPLE_MORPHEME_FAMILY_1385,
+    EXAMPLE_NULL_BREAKDOWN_ITEM_1,
+    seed_data,
+)
 
-# from  rootski_api.tests.fixtures.rootski_dynamo_table import rootski_dynamo_table, dynamo_db_service
-
+# from rootski_api.tests.fixtures.rootski_dynamo_table import rootski_dynamo_table, dynamo_db_service
 # from rootski.services.database.dynamo.models2schemas.word import dynamo_to_pydantic__word
 # from rootski.services.database.dynamo.actions.dynamo import get_item_from_dynamo_response, get_item_status_code
 # from rootski import schemas
 
-# Constants
-BREAKDOWN_INDEX = 0
-BREAKDOWN_VERIFIED_INDEX = 2
-BREAKDOWN_ITEM_INDEX = 3
-BREAKDOWN_ITEM_NULL_INDEX = 4
-
 
 def test__breakdown_from_dict():
+    breakdown = Breakdown.from_dict(breakdown_dict=EXAMPLE_BREAKDOWN)
 
-    breakdown = Breakdown.from_dict(breakdown_dict=EXAMPLE_DATA[BREAKDOWN_INDEX])
-
-    assert breakdown.pk == EXAMPLE_DATA[BREAKDOWN_INDEX]["pk"]
-    assert breakdown.sk == EXAMPLE_DATA[BREAKDOWN_INDEX]["sk"]
-    assert breakdown.gsi1pk == EXAMPLE_DATA[BREAKDOWN_INDEX]["gsi1pk"]
-    assert breakdown.gsi1sk == EXAMPLE_DATA[BREAKDOWN_INDEX]["gsi1sk"]
-    assert breakdown.gsi2pk == EXAMPLE_DATA[BREAKDOWN_INDEX]["gsi2pk"]
-    assert breakdown.gsi2sk == EXAMPLE_DATA[BREAKDOWN_INDEX]["gsi2sk"]
-    assert breakdown.word == EXAMPLE_DATA[BREAKDOWN_INDEX]["word"]
-    assert breakdown.word_id == EXAMPLE_DATA[BREAKDOWN_INDEX]["word_id"]
-    assert breakdown.submitted_by_user_email == EXAMPLE_DATA[BREAKDOWN_INDEX]["submitted_by_user_email"]
-    assert breakdown.is_verified == EXAMPLE_DATA[BREAKDOWN_INDEX]["is_verified"]
-    assert breakdown.is_inference == EXAMPLE_DATA[BREAKDOWN_INDEX]["is_inference"]
-    assert breakdown.date_submitted == EXAMPLE_DATA[BREAKDOWN_INDEX]["date_submitted"]
-    assert breakdown.date_verified == EXAMPLE_DATA[BREAKDOWN_INDEX]["date_verified"]
+    assert breakdown.pk == EXAMPLE_BREAKDOWN["pk"]
+    assert breakdown.sk == EXAMPLE_BREAKDOWN["sk"]
+    assert breakdown.gsi1pk == EXAMPLE_BREAKDOWN["gsi1pk"]
+    assert breakdown.gsi1sk == EXAMPLE_BREAKDOWN["gsi1sk"]
+    assert breakdown.gsi2pk == EXAMPLE_BREAKDOWN["gsi2pk"]
+    assert breakdown.gsi2sk == EXAMPLE_BREAKDOWN["gsi2sk"]
+    assert breakdown.word == EXAMPLE_BREAKDOWN["word"]
+    assert breakdown.word_id == EXAMPLE_BREAKDOWN["word_id"]
+    assert breakdown.submitted_by_user_email == EXAMPLE_BREAKDOWN["submitted_by_user_email"]
+    assert breakdown.is_verified == EXAMPLE_BREAKDOWN["is_verified"]
+    assert breakdown.is_inference == EXAMPLE_BREAKDOWN["is_inference"]
+    assert breakdown.date_submitted == EXAMPLE_BREAKDOWN["date_submitted"]
+    assert breakdown.date_verified == EXAMPLE_BREAKDOWN["date_verified"]
 
     # Fix this test for the remaining breakdown items
 
@@ -51,49 +58,78 @@ def test__breakdown_from_dict():
 def test__breakdown_item_from_dict():
     # Null breakdown item
     null_breakdown_item = make_dynamo_breakdown_item_from_dict(
-        breakdown_item_dict=EXAMPLE_DATA[BREAKDOWN_ITEM_NULL_INDEX]
+        breakdown_item_dict=EXAMPLE_NULL_BREAKDOWN_ITEM_1
     )
-    assert null_breakdown_item.word_id == EXAMPLE_DATA[BREAKDOWN_ITEM_NULL_INDEX]["word_id"]
-    assert null_breakdown_item.position == EXAMPLE_DATA[BREAKDOWN_ITEM_NULL_INDEX]["position"]
-    assert null_breakdown_item.morpheme == EXAMPLE_DATA[BREAKDOWN_ITEM_NULL_INDEX]["morpheme"]
+    assert null_breakdown_item.word_id == EXAMPLE_NULL_BREAKDOWN_ITEM_1["word_id"]
+    assert null_breakdown_item.position == EXAMPLE_NULL_BREAKDOWN_ITEM_1["position"]
+    assert null_breakdown_item.morpheme == EXAMPLE_NULL_BREAKDOWN_ITEM_1["morpheme"]
     assert (
-        null_breakdown_item.submitted_by_user_email
-        == EXAMPLE_DATA[BREAKDOWN_ITEM_NULL_INDEX]["submitted_by_user_email"]
+        null_breakdown_item.submitted_by_user_email == EXAMPLE_NULL_BREAKDOWN_ITEM_1["submitted_by_user_email"]
     )
 
     # Breakdown Item
-    breakdown_item = make_dynamo_breakdown_item_from_dict(
-        breakdown_item_dict=EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]
-    )
-    assert breakdown_item.word_id == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["word_id"]
-    assert breakdown_item.position == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["position"]
-    assert breakdown_item.morpheme == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["morpheme"]
-    assert (
-        breakdown_item.submitted_by_user_email == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["submitted_by_user_email"]
-    )
-    assert breakdown_item.morpheme_id == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["morpheme_id"]
-    assert breakdown_item.morpheme_family_id == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["morpheme_family_id"]
-    assert breakdown_item.breakdown_id == EXAMPLE_DATA[BREAKDOWN_ITEM_INDEX]["breakdown_id"]
+    breakdown_item = make_dynamo_breakdown_item_from_dict(breakdown_item_dict=EXAMPLE_BREAKDOWN_ITEM_1)
+    assert breakdown_item.word_id == EXAMPLE_BREAKDOWN_ITEM_1["word_id"]
+    assert breakdown_item.position == EXAMPLE_BREAKDOWN_ITEM_1["position"]
+    assert breakdown_item.morpheme == EXAMPLE_BREAKDOWN_ITEM_1["morpheme"]
+    assert breakdown_item.submitted_by_user_email == EXAMPLE_BREAKDOWN_ITEM_1["submitted_by_user_email"]
+    assert breakdown_item.morpheme_id == EXAMPLE_BREAKDOWN_ITEM_1["morpheme_id"]
+    assert breakdown_item.morpheme_family_id == EXAMPLE_BREAKDOWN_ITEM_1["morpheme_family_id"]
+    assert breakdown_item.breakdown_id == EXAMPLE_BREAKDOWN_ITEM_1["breakdown_id"]
 
 
 def test__get_official_breakdown_by_word_id(dynamo_db_service: DBService):
     seed_data(rootski_dynamo_table=dynamo_db_service.rootski_table)
-    breakdown = get_official_breakdown_by_word_id(word_id="32", db=dynamo_db_service)
-    assert breakdown.word == EXAMPLE_DATA[BREAKDOWN_INDEX]["word"]
-    assert breakdown.is_verified == EXAMPLE_DATA[BREAKDOWN_INDEX]["is_verified"]
+    word_id = "32"
+
+    breakdown: Breakdown = get_official_breakdown_by_word_id(word_id=word_id, db=dynamo_db_service)
+    assert breakdown.word == EXAMPLE_BREAKDOWN["word"]
+    assert breakdown.is_verified == EXAMPLE_BREAKDOWN["is_verified"]
 
 
-def test__is_verified(dynamo_db_service: DBService):
+def test__is_breakdown_verified(dynamo_db_service: DBService):
     seed_data(rootski_dynamo_table=dynamo_db_service.rootski_table)
-    breakdown = get_official_breakdown_by_word_id(word_id="32", db=dynamo_db_service)
-    assert is_verified(breakdown=breakdown) is False
+    word_id = "32"
+
+    breakdown: Breakdown = get_official_breakdown_by_word_id(word_id=word_id, db=dynamo_db_service)
+    assert is_breakdown_verified(breakdown=breakdown) is False
 
 
-# if __name__ == "__main__":
-#     db = DBService("rootski-table")
-#     db.init()
-#     table = db.rootski_table
-#     get_item_response = table.get_item(
-#         Key={"pk": "WORD#771", "sk": "BREAKDOWN_ITEM#2213#0"}
-#     )  # eric.riddoch@gmail.com"})
-#     print(get_item_response["Item"])
+def test__get_breakdown_submitted_by_user_email_and_word_id(dynamo_db_service: DBService):
+    seed_data(rootski_dynamo_table=dynamo_db_service.rootski_table)
+    user_email = "eric.riddoch@gmail.com"
+    word_id = "5"
+
+    breakdown: Breakdown = get_breakdown_submitted_by_user_email_and_word_id(
+        user_email=user_email, word_id=word_id, db=dynamo_db_service
+    )
+    assert breakdown.word_id == EXAMPLE_BREAKDOWN_ERIC_USER["word_id"]
+    assert breakdown.submitted_by_user_email == user_email
+    assert breakdown.submitted_by_user_email != "anonymous"
+    assert breakdown.submitted_by_user_email != "another_user@gmail.com"
+
+
+def test__get_official_breakdown_submitted_by_another_user(dynamo_db_service: DBService) -> Breakdown:
+    seed_data(rootski_dynamo_table=dynamo_db_service.rootski_table)
+    word_id = "10"
+    user_email = "eric.riddoch@gmail.com"
+
+    breakdown: Breakdown = get_official_breakdown_submitted_by_another_user(
+        word_id=word_id, db=dynamo_db_service
+    )
+    assert breakdown.submitted_by_user_email == EXAMPLE_BREAKDOWN_ANOTHER_USER["submitted_by_user_email"]
+    assert breakdown.submitted_by_user_email != user_email
+    assert breakdown.submitted_by_user_email != "anonymous"
+    assert breakdown.submitted_by_user_email != "null"
+
+
+def test__get_morpheme_family(dynamo_db_service: DBService) -> MorphemeFamily:
+    seed_data(rootski_dynamo_table=dynamo_db_service.rootski_table)
+    morpheme_family_id = "245"
+
+    morpheme_family: MorphemeFamily = get_morpheme_family(
+        morpheme_family_id=morpheme_family_id, db=dynamo_db_service
+    )
+
+    assert morpheme_family.family_id == morpheme_family_id
+    assert morpheme_family.family_meanings == EXAMPLE_MORPHEME_FAMILY_245["family_meanings"]
